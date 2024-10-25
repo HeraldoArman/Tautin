419631131e817176e33589cfb67d7d6013a2ccad
HeraldoArman <heraldoarman7@gmail.com>
2024-10-25 19:23:25 +0700

    add edit page

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const paragraf = document.querySelector('.deskripsi'); // Menggunakan querySelector dengan class
+    const paragraf = document.querySelector('.deskripsi'); 
     if (paragraf) {
         console.log('Paragraf ditemukan!');
         const kalimat = paragraf.innerHTML;
         
-        // Pisahkan teks menjadi kata-kata dan bungkus dengan <span>
+
         const kataBerSpan = kalimat.split(' ').map(kata => `<span>${kata}</span>`).join(' ');
 
-        // Ganti konten paragraf dengan teks yang sudah diubah
         paragraf.innerHTML = kataBerSpan;
     } else {
         console.error('Paragraf tidak ditemukan!');
@@ -16,25 +15,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.querySelectorAll('.copy-icon').forEach(icon => {
     icon.addEventListener('click', function() {
-        // Cari elemen .short-link terdekat dengan ikon yang diklik
         let linkText = icon.previousElementSibling.textContent;
-        
-        // Buat elemen input sementara
         let tempInput = document.createElement('input');
+
         tempInput.value = linkText;
         document.body.appendChild(tempInput);
-        
-        // Pilih teks dan salin ke clipboard
         tempInput.select();
         document.execCommand('copy');
-        
-        // Hapus elemen input sementara
+
         document.body.removeChild(tempInput);
-        
-        // Tambahkan kelas "active" ke ikon dan tampilkan teks
+
         icon.classList.add('active');
-        
-        // Hapus kelas "active" setelah 1 detik
         setTimeout(function() {
             icon.classList.remove('active');
         }, 3000);
