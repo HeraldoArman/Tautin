71434f596c3454d8ade6ec423843067523f3d29e
HeraldoArman <heraldoarman7@gmail.com>
2024-10-25 16:33:25 +0700

    add copy feature

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ document.addEventListener('DOMContentLoaded', function() {
         console.error('Paragraf tidak ditemukan!');
     }
 });
+
+document.querySelectorAll('.copy-icon').forEach(icon => {
+    icon.addEventListener('click', function() {
+        // Cari elemen .short-link terdekat dengan ikon yang diklik
+        let linkText = icon.previousElementSibling.textContent;
+        
+        // Buat elemen input sementara
+        let tempInput = document.createElement('input');
+        tempInput.value = linkText;
+        document.body.appendChild(tempInput);
+        
+        // Pilih teks dan salin ke clipboard
+        tempInput.select();
+        document.execCommand('copy');
+        
+        // Hapus elemen input sementara
+        document.body.removeChild(tempInput);
+        
+        // Tambahkan kelas "active" ke ikon dan tampilkan teks
+        icon.classList.add('active');
+        
+        // Hapus kelas "active" setelah 1 detik
+        setTimeout(function() {
+            icon.classList.remove('active');
+        }, 3000);
+    });
+});
